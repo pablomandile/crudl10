de20eee29b979d8ef52ceddab0269e6a8fa02661
pablo.mandile@bue.edu.ar <pablo.mandile@bue.edu.ar>
2023-06-16 14:22:28 -0300

    creación del proyecto empleados y departamentos crud

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Department;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $departments = Department::all();
+        return Inertia::render('Departments\index', ['departments' => $departments]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Departments\create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate(['name' => 'required|max:100']);
+        $department = new Department($request->input());
+        $department->save();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Department $department)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Department $department)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Department $department)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Department $department)
+    {
+        //
+    }
+}
